7ca00dc29043e1270d4c45d1453dad5aa3bac55b
petri.tuononen <petri.tuononen@9ead2519-7523-cc65-c473-fb31773be507>
2011-05-09 12:03:27 +0000

    More classes.
    git-svn-id: http://pkg-config-support-for-eclipse-cdt.googlecode.com/svn/trunk@15 9ead2519-7523-cc65-c473-fb31773be507

---
+++ src/org/eclipse/cdt/managedbuilder/pkgconfig/isPkgConfigSupported.java
@@ -0,0 +1,5 @@
+package org.eclipse.cdt.managedbuilder.pkgconfig;
+
+public class isPkgConfigSupported {
+
+}
---
+++ src/org/eclipse/cdt/managedbuilder/pkgconfig/preferences/PreferenceStore.java
@@ -0,0 +1,5 @@
+package org.eclipse.cdt.managedbuilder.pkgconfig.preferences;
+
+public class PreferenceStore {
+
+}
