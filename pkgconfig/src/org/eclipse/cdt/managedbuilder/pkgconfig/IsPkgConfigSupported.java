b00da4bb57daec1e594683a0006bb837eb38d326
petri.tuononen <petri.tuononen@9ead2519-7523-cc65-c473-fb31773be507>
2011-05-10 13:35:36 +0000

    Copyright notifications.
    git-svn-id: http://pkg-config-support-for-eclipse-cdt.googlecode.com/svn/trunk@24 9ead2519-7523-cc65-c473-fb31773be507

--- pkgconfig/src/org/eclipse/cdt/managedbuilder/pkgconfig/IsPkgConfigSupported.java
+++ pkgconfig/src/org/eclipse/cdt/managedbuilder/pkgconfig/IsPkgConfigSupported.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Petri Tuononen and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Petri Tuononen - Initial implementation
+ *******************************************************************************/
 package org.eclipse.cdt.managedbuilder.pkgconfig;
 
 public class IsPkgConfigSupported {
