2e22422cd9f0ea8808557ceac904e6b69944e044
petri.tuononen <petri.tuononen@9ead2519-7523-cc65-c473-fb31773be507>
2011-06-05 07:18:09 +0000

    git-svn-id: http://pkg-config-support-for-eclipse-cdt.googlecode.com/svn/trunk@103 9ead2519-7523-cc65-c473-fb31773be507

--- pkgconfig/src/org/eclipse/cdt/managedbuilder/pkgconfig/util/ArrayUtil.java
+++ pkgconfig/src/org/eclipse/cdt/managedbuilder/pkgconfig/util/ArrayUtil.java
@@ -22,7 +22,7 @@ public class ArrayUtil {
 	 * @return String array containing multiple paths.
 	 */
 	public static String[] stringToArray(String str) {
-		return str.split(System.getProperty("path.separator")); //$NON-NLS-1$
+		return str.split(Separators.getPathSeparator()); //$NON-NLS-1$
 	}
 
 	/**
@@ -38,7 +38,7 @@ public class ArrayUtil {
 		if (array.length>0 /*&& !array[0].equals("")*/) {
 			for (String i : array) {
 				sB.append(i);
-				sB.append(System.getProperty("path.separator")); //$NON-NLS-1$
+				sB.append(Separators.getPathSeparator()); //$NON-NLS-1$
 			}			
 		}
 		return sB.toString();
