f5a3a34037e5088716a87c15520e29fc7394e0a5
petri.tuononen <petri.tuononen@9ead2519-7523-cc65-c473-fb31773be507>
2012-07-22 16:34:05 +0000

    Fixed issue 10: "-l" is removed from library names while importing.
    git-svn-id: http://pkg-config-support-for-eclipse-cdt.googlecode.com/svn/trunk@216 9ead2519-7523-cc65-c473-fb31773be507

--- org.eclipse.cdt.managedbuilder.pkgconfig/src/org/eclipse/cdt/managedbuilder/pkgconfig/util/Parser.java
+++ org.eclipse.cdt.managedbuilder.pkgconfig/src/org/eclipse/cdt/managedbuilder/pkgconfig/util/Parser.java
@@ -80,29 +80,29 @@ public class Parser {
 		return null;
 	}
 	
-	/**
-	 * Parses library search paths from "pkg-config --libs" input.
-	 * 
-	 * @param s Output from pkg-config.
-	 * @return Parsed String array.
-	 */
-	public static String[] parseLibPaths(String s) throws NullPointerException {
-		//find the index where library path list starts
-		int start = s.indexOf("-L"); //$NON-NLS-1$
-		String str;
-		if (start != -1) { //if library paths found
-			//find the index where library list starts
-			int end = s.indexOf(" -l"); //$NON-NLS-1$
-			//truncate other than library paths
-			str = s.substring(start, end);
-			//remove library search path flags
-			str = str.replace("-L", ""); //$NON-NLS-1$ //$NON-NLS-2$
-			//insert lib paths into an array
-			String[] libPaths = str.split(" "); //$NON-NLS-1$
-			return libPaths;
-		}
-		return null;
-	}
+//	/**
+//	 * Parses library search paths from "pkg-config --libs" input.
+//	 * 
+//	 * @param s Output from pkg-config.
+//	 * @return Parsed String array.
+//	 */
+//	public static String[] parseLibPaths(String s) throws NullPointerException {
+//		//find the index where library path list starts
+//		int start = s.indexOf("-L"); //$NON-NLS-1$
+//		String str;
+//		if (start != -1) { //if library paths found
+//			//find the index where library list starts
+//			int end = s.indexOf(" -l"); //$NON-NLS-1$
+//			//truncate other than library paths
+//			str = s.substring(start, end);
+//			//remove library search path flags
+//			str = str.replace("-L", ""); //$NON-NLS-1$ //$NON-NLS-2$
+//			//insert lib paths into an array
+//			String[] libPaths = str.split(" "); //$NON-NLS-1$
+//			return libPaths;
+//		}
+//		return null;
+//	}
 	
 	/**
 	 * Parses library search paths from "pkg-config --libs-only-L" input.
@@ -121,33 +121,33 @@ public class Parser {
 		return null;
 	}
 	
-	/**
-	 * Parses libraries from "pkg-config --libs" input.
-	 * 
-	 * @param s Output from pkg-config.
-	 * @return Parsed String array.
-	 */
-	public static String[] parseLibs(String s) throws NullPointerException {
-		if (s != null) {
-			String str;
-			//special case if pkg-config --libs output starts with -l
-			int start = s.indexOf("-l"); //$NON-NLS-1$
-			if (start != 0) {
-				start = s.indexOf(" -l"); //$NON-NLS-1$
-			}
-			if (start != -1) { //if libraries found
-				//truncate library search paths
-				str = s.substring(start+1, s.length()-1);
-				//remove lib flags
-				str = str.replace("-l", ""); //$NON-NLS-1$ //$NON-NLS-2$
-				//insert libs into an array
-				String[] libs = str.split(" "); //$NON-NLS-1$
-				return libs;
-			}
-			return null;			
-		}
-		return null;	
-	}
+//	/**
+//	 * Parses libraries from "pkg-config --libs" input.
+//	 * 
+//	 * @param s Output from pkg-config.
+//	 * @return Parsed String array.
+//	 */
+//	public static String[] parseLibs(String s) throws NullPointerException {
+//		if (s != null) {
+//			String str;
+//			//special case if pkg-config --libs output starts with -l
+//			int start = s.indexOf("-l"); //$NON-NLS-1$
+//			if (start != 0) {
+//				start = s.indexOf(" -l"); //$NON-NLS-1$
+//			}
+//			if (start != -1) { //if libraries found
+//				//truncate library search paths
+//				str = s.substring(start+1, s.length()-1);
+//				//remove lib flags
+//				str = str.replace(" -l", " "); //$NON-NLS-1$ //$NON-NLS-2$
+//				//insert libs into an array
+//				String[] libs = str.split(" "); //$NON-NLS-1$
+//				return libs;
+//			}
+//			return null;			
+//		}
+//		return null;	
+//	}
 	
 	/**
 	 * Parses libraries from "pkg-config --libs-only-l" input.
@@ -156,12 +156,17 @@ public class Parser {
 	 * @return Parsed String array.
 	 */
 	public static String[] parseLibs2(String s) throws NullPointerException {
-		if (s != null) {
+		if (s != null) {		
+			String libStr = s;
+			if(libStr.startsWith("-l")) { //$NON-NLS-1$
+				libStr = libStr.replaceFirst("-l", ""); //$NON-NLS-1$ //$NON-NLS-2$
+				//libStr = libStr.substring(2, libStr.length()-1);
+			}
 			//remove lib flags
-			String s2 = s.replace("-l", ""); //$NON-NLS-1$ //$NON-NLS-2$
+			libStr = libStr.replace(" -l", " "); //$NON-NLS-1$ //$NON-NLS-2$
 			//insert libs into an array
-			String[] libs = s2.split(" "); //$NON-NLS-1$
-			return libs;
+			String[] libs = libStr.split(" "); //$NON-NLS-1$
+		    return libs;
 		}
 		return null;
 	}
